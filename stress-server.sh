09010fa0093bee944738b728bf3277961d9bd6d7
José Bollo <jose.bollo@iot.bzh>
2017-04-12 17:56:10 +0200

    add argument to stress-server.sh
    Change-Id: I75fccf6c9b5c37c7548019e510f7c3281a5cd664
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- stress-server.sh
+++ stress-server.sh
@@ -16,10 +16,10 @@ ARGS="-q --session-max=100 --port=$PORT --workdir=$ROOT --roothttp=$TEST --token
 
 echo -n launch afb...
 case "$1" in
- gdb) gdb $AFB -ex "run $ARGS";;
- valgrind) valgrind --leak-check=full $AFB $ARGS 2>&1 | tee $OUT;;
- strace) strace -tt -f -o $OUT.strace $AFB $ARGS 2>&1 | tee $OUT;;
- *) $AFB $ARGS 2>&1 | tee $OUT;;
+ gdb) shift; gdb $AFB -ex "run $ARGS $@";;
+ valgrind) shift; valgrind --leak-check=full $AFB $ARGS $@ 2>&1 | tee $OUT;;
+ strace) shift; strace -tt -f -o $OUT.strace $AFB $ARGS $@ 2>&1 | tee $OUT;;
+ *) $AFB $ARGS $@ 2>&1 | tee $OUT;;
 esac
 #$AFB -q --session-max=100 --port=$PORT --workdir=$ROOT --roothttp=$TEST --token=$TOKEN --ldpaths=/tmp --binding=$HELLO  > $OUT.0 2>&1 &
 #afbpid=$!
