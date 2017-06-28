dff4f960481fc5916ac1c19ebbd38e8023406309
José Bollo <jose.bollo@iot.bzh>
2017-06-28 15:35:01 +0200

    Use constants for verbosity and syslog levels
    Change-Id: I98a765f7c40d77fbc565ed13aa6e9bba48cf3244
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

---
+++ include/afb/afb-verbosity.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016, 2017 "IoT.bzh"
+ * Author: José Bollo <jose.bollo@iot.bzh>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#define AFB_VERBOSITY_LEVEL_ERROR	0
+#define AFB_VERBOSITY_LEVEL_WARNING	1
+#define AFB_VERBOSITY_LEVEL_NOTICE	1
+#define AFB_VERBOSITY_LEVEL_INFO	2
+#define AFB_VERBOSITY_LEVEL_DEBUG	3
+
+#define _AFB_SYSLOG_LEVEL_ERROR_	3
+#define _AFB_SYSLOG_LEVEL_WARNING_	4
+#define _AFB_SYSLOG_LEVEL_NOTICE_	5
+#define _AFB_SYSLOG_LEVEL_INFO_		6
+#define _AFB_SYSLOG_LEVEL_DEBUG_	7
+
