dbb880d5cfab46eba2a3594521c5847fa74c3b70
José Bollo <jose.bollo@iot.bzh>
2017-11-06 13:07:34 +0100

    AFB.js: make HELLO the default token
    Change-Id: I76ae307672e1268b9cc4a9194e61f884a3f7455b
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- test/monitoring/AFB.js
+++ test/monitoring/AFB.js
@@ -21,7 +21,7 @@ if (typeof base != "object")
 
 var initial = {
 	base: base.base || "api",
-	token: base.token || initialtoken || "hello",
+	token: base.token || initialtoken || "HELLO",
 	host: base.host || window.location.host,
 	url: base.url || undefined
 };
